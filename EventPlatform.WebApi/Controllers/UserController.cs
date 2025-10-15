9c0b22d18d1b40a15f4416f78c72ed47a5ee8040
kleqing <nhanpk28122004@gmail.com>
2025-10-15 22:46:15 +0700

    [[Update] Add api get me to check authorization for FE

--- EventPlatform.WebApi/Controllers/UserController.cs
+++ EventPlatform.WebApi/Controllers/UserController.cs
@@ -1,7 +1,9 @@
-﻿using EventPlatform.Application.Common;
+﻿using System.Security.Claims;
+using EventPlatform.Application.Common;
 using EventPlatform.Application.Contracts.Requests;
 using EventPlatform.Application.Services.Interfaces.Auth;
 using EventPlatform.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EventPlatform.WebApi.Controllers;
@@ -17,25 +19,12 @@ public class UserController : ControllerBase
         _authServices = authServices;
     }
 
-    // [HttpPost("register")]
-    // public async Task<IActionResult> Register([FromBody] RegisterRequest request)
-    // {
-    //     var response = new BaseResultResponse<User>();
-    //     try
-    //     {
-    //         var user = await _authServices.Register(request);
-    //         response.StatusCode = 200;
-    //         response.Success = true;
-    //         response.Message = "User registered successfully.";
-    //         response.Data = user;
-    //         return Ok(response);
-    //     }
-    //     catch (Exception ex)
-    //     {
-    //         response.StatusCode = 400;
-    //         response.Success = false;
-    //         response.Message = ex.Message;
-    //         return BadRequest(response);
-    //     }
-    // }
+    //* Don't delete this action, it's used to response to the client that is the user is logged in
+    [Authorize]
+    [HttpGet("me")]
+    public IActionResult Me()
+    {
+        var userEmail = User.FindFirst(ClaimTypes.Email)?.Value;
+        return Ok(new { email = userEmail });
+    }
 }
