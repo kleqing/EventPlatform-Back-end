df975dbbe336c82f910b663b16960cbb4090b0b5
kleqing <nhanpk28122004@gmail.com>
2025-10-15 10:23:18 +0700

    [Add] Add url helper to get fe and be string from .env or appsettings.json

---
+++ EventPlatform.Shared/Utils/UrlHelper.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace EventPlatform.Shared.Utils;
+
+public class UrlHelper
+{
+    public static string GetBackendUrl(IConfiguration configuration) 
+        => Environment.GetEnvironmentVariable("BACKEND_URL") ?? configuration["URLs:BackendURL"] ?? string.Empty;
+    
+    public static string GetFrontendUrl(IConfiguration configuration) 
+        => Environment.GetEnvironmentVariable("FRONTEND_URL") ?? configuration["URLs:FrontendURL"] ?? string.Empty;
+}
