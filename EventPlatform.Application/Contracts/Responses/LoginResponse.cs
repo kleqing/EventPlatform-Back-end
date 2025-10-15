c8bd4c7dfe17cae7dc3ec8cf79c19bc47a73f060
kleqing <nhanpk28122004@gmail.com>
2025-10-15 10:22:24 +0700

    [Update] Response

---
+++ EventPlatform.Application/Contracts/Responses/LoginResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace EventPlatform.Application.Contracts.Responses;
+
+public class LoginResponse
+{
+    public string? RefreshToken { get; set; }
+    public DateTime? RefreshTokenExpiryTime { get; set; }
+}
