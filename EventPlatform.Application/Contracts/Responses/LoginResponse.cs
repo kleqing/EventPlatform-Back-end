33b44f0470673b513f8c8865faf30179d8b0acc4
kleqing <nhanpk28122004@gmail.com>
2025-10-15 22:45:20 +0700

    [Update] Add more field for response

--- EventPlatform.Application/Contracts/Responses/LoginResponse.cs
+++ EventPlatform.Application/Contracts/Responses/LoginResponse.cs
@@ -1,7 +1,14 @@
-﻿namespace EventPlatform.Application.Contracts.Responses;
+﻿using EventPlatform.Domain.Enums;
+
+namespace EventPlatform.Application.Contracts.Responses;
 
 public class LoginResponse
 {
+    public Guid UserId { get; set; }
+    public string Email { get; set; }
+    public string FullName { get; set; }
+    public string UserName { get; set; }
+    public UserRole Role { get; set; }
     public string? RefreshToken { get; set; }
     public DateTime? RefreshTokenExpiryTime { get; set; }
 }
