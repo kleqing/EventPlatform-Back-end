76a3e6832d5dffbc76e3b6cc34944bba570f5611
kleqing <nhanpk28122004@gmail.com>
2025-10-16 20:56:40 +0700

    [Update] Update check string is empty or null in environment variable

--- EventPlatform.Shared/Utils/UrlHelper.cs
+++ EventPlatform.Shared/Utils/UrlHelper.cs
@@ -4,9 +4,31 @@ namespace EventPlatform.Shared.Utils;
 
 public class UrlHelper
 {
-    public static string GetBackendUrl(IConfiguration configuration) 
-        => Environment.GetEnvironmentVariable("BACKEND_URL") ?? configuration["URLs:BackendURL"] ?? string.Empty;
-    
-    public static string GetFrontendUrl(IConfiguration configuration) 
-        => Environment.GetEnvironmentVariable("FRONTEND_URL") ?? configuration["URLs:FrontendURL"] ?? string.Empty;
+    public static string GetBackendUrl(IConfiguration configuration)
+    {
+        var backendUrl = Environment.GetEnvironmentVariable("BACKEND_URL");
+        if (string.IsNullOrWhiteSpace(backendUrl))
+        {
+            backendUrl = configuration["URLs:BackendURL"];
+            if (string.IsNullOrWhiteSpace(backendUrl))
+            {
+                throw new Exception("Backend URL not configured");
+            }
+        }
+        return backendUrl;
+    }
+
+    public static string GetFrontendUrl(IConfiguration configuration)
+    {
+        var frontendUrl = Environment.GetEnvironmentVariable("FRONTEND_URL");
+        if (string.IsNullOrWhiteSpace(frontendUrl))
+        {
+            frontendUrl = configuration["URLs:FrontendURL"];
+            if (string.IsNullOrWhiteSpace(frontendUrl))
+            {
+                throw new Exception("Frontend URL not configured");
+            }
+        }
+        return frontendUrl;
+    }
 }
