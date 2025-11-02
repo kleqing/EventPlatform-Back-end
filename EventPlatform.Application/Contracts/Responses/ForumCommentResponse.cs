9ff4a56d3e491a3cd307471126db6442f6448d6c
kleqing <nhanpk28122004@gmail.com>
2025-11-02 17:44:34 +0700

    [Add] Add reponse comment

---
+++ EventPlatform.Application/Contracts/Responses/ForumCommentResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace EventPlatform.Application.Contracts.Responses;
+
+public class ForumCommentResponse
+{
+    public int CommentId { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public int LikeCount { get; set; }
+    public bool IsLikedByCurrentUser { get; set; }
+}
