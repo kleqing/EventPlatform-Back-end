df43adf6fec190bed0932f826143f21f85e50338
kleqing <nhanpk28122004@gmail.com>
2025-11-02 20:55:32 +0700

    [Update] Remove connection string

--- EventPlatform.Infrastructure/Data/ApplicationDbContext.cs
+++ EventPlatform.Infrastructure/Data/ApplicationDbContext.cs
@@ -43,11 +43,7 @@ public partial class ApplicationDbContext : DbContext
     public virtual DbSet<Transaction> Transactions { get; set; }
 
     public virtual DbSet<User> Users { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=db8300.public.databaseasp.net; Database=db8300; User Id=db8300; Password=3i!Em@Y5_8bH; Encrypt=True; TrustServerCertificate=True; MultipleActiveResultSets=True;");
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Connection>(entity =>
