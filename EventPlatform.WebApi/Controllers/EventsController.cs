7c9b35477c9ece49c2d1093f649a380063dfc977
minhphamm1309 <minhphamm1309@gmail.com>
2025-11-19 23:15:27 +0700

    update: add call fast api after create event

--- EventPlatform.WebApi/Controllers/EventsController.cs
+++ EventPlatform.WebApi/Controllers/EventsController.cs
@@ -5,6 +5,7 @@ using EventPlatform.Application.Contracts.Dtos;
 using EventPlatform.Application.Contracts.Requests;
 using EventPlatform.Application.Services.Interfaces.Event;
 using EventPlatform.Domain.Entities;
+using EventPlatform.Shared.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,10 +17,14 @@ namespace EventPlatform.WebApi.Controllers
     public class EventsController : ControllerBase
     {
         private readonly IEventService _eventService;
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
 
-        public EventsController(IEventService eventService)
+        public EventsController(IEventService eventService, HttpClient httpClient, IConfiguration configuration)
         {
             _eventService = eventService;
+            _httpClient = httpClient;
+            _configuration = configuration;
         }
 
         /// <param name="query">Tham số tìm kiếm, lọc, phân trang</param>
@@ -92,6 +97,14 @@ namespace EventPlatform.WebApi.Controllers
                     SaleEndDate = t.SaleEndDate,
                 }).ToList();
                 await _eventService.CreateTicketTypes(newTicketTypes);
+                // Lấy Base URL từ cấu hình
+                var fastApiBaseUrl = UrlHelper.GetFastAPIUrl(_configuration); ;
+
+                // Dựng URL cho action UPSERT
+                var fastApiUrl = $"{fastApiBaseUrl}/internal/events/manage/{newEvent.EventId}?action=UPSERT";
+
+                // Gửi yêu cầu POST (không cần body vì FastAPI sẽ tự lấy data từ CSDL)
+                var response = await _httpClient.PostAsync(fastApiUrl, null);
             }
             catch (Exception ex)
             {
@@ -145,30 +158,30 @@ namespace EventPlatform.WebApi.Controllers
             }
         }
 
-        [Authorize]
-        [HttpGet("applied")]
-        public async Task<IActionResult> GetAppliedEvents()
-        {
-            var response = new BaseResultResponse<AppliedEventsGroupedDto>();
+        //[Authorize]
+        //[HttpGet("applied")]
+        //public async Task<IActionResult> GetAppliedEvents()
+        //{
+        //    var response = new BaseResultResponse<AppliedEventsGroupedDto>();
 
-            try
-            {
-                var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-                if (string.IsNullOrEmpty(userId))
-                {
-                    response.StatusCode = StatusCodes.Status401Unauthorized;
-                    response.Success = false;
-                    response.Message = "User not found in token.";
-                    return StatusCode(StatusCodes.Status401Unauthorized, response);
-                }
+        //    try
+        //    {
+        //        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        //        if (string.IsNullOrEmpty(userId))
+        //        {
+        //            response.StatusCode = StatusCodes.Status401Unauthorized;
+        //            response.Success = false;
+        //            response.Message = "User not found in token.";
+        //            return StatusCode(StatusCodes.Status401Unauthorized, response);
+        //        }
 
-                var userGuidId = Guid.Parse(userId);
-                var appliedEvents = await _eventService.GetAppliedEventsAsync(userGuidId);
+        //        var userGuidId = Guid.Parse(userId);
+        //        var appliedEvents = await _eventService.GetAppliedEventsAsync(userGuidId);
 
-                response.StatusCode = StatusCodes.Status200OK;
-                response.Success = true;
-                response.Message = "Applied events retrieved successfully.";
-                response.Data = appliedEvents;
+        //        response.StatusCode = StatusCodes.Status200OK;
+        //        response.Success = true;
+        //        response.Message = "Applied events retrieved successfully.";
+        //        response.Data = appliedEvents;
 
                 return Ok(response);
             }
